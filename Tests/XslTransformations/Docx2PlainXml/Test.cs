54489f7bac5d72ac2c6b15cc8c326c5093e7ac02
Michael <darth.michael.dlots@gmail.com>
2011-08-04 18:14:26 +0200

    temp commit.

--- Tests/XslTransformations/Docx2PlainXml/Test.cs
+++ Tests/XslTransformations/Docx2PlainXml/Test.cs
@@ -1,24 +1,16 @@
 using System;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
 using System.Reflection;
-using System.Xml.Linq;
 using DocxConverter.XslTransformations;
-using Jolt.Testing.Assertions.NUnit.SyntaxHelpers;
 using NUnit.Framework;
 
 namespace Tests.XslTransformations.Docx2PlainXml
 {
   [TestFixture]
-  public class Test
+  public class Test : TestBase
   {
-    private XslTransformation _transformation;
-
-    [SetUp]
-    public void SetUp ()
+    protected override XslTransformation CreateXslTransformation ()
     {
-      _transformation = CreateXslTransformation ("Docx2PlainXml.xslt");
+      return CreateXslTransformation ("Docx2PlainXml.xslt");
     }
 
     [Test]
@@ -42,58 +34,13 @@ namespace Tests.XslTransformations.Docx2PlainXml
     [Test]
     public void EmptyTags ()
     {
-      RunAssertion (MethodBase.GetCurrentMethod ());
+      RunAssertion (MethodBase.GetCurrentMethod());
     }
 
     [Test]
     public void Integration ()
     {
       RunAssertion (MethodBase.GetCurrentMethod());
-    }
-
-    private void RunAssertion (MethodBase currentMethod)
-    {
-      var source = GetSourceXml (currentMethod);
-      var expected = GetResultXml (currentMethod);
-      var actual = _transformation.Transform (source);
-      using (var actualReader = actual.CreateReader())
-      {
-        using (var expectedReader = expected.CreateReader())
-        {
-          Assert.That (actualReader, IsXml.EquivalentTo (expectedReader));
-        }
-      }
-    }
-
-    private XslTransformation CreateXslTransformation (string fileName)
-    {
-      var fullPath = Path.Combine ("XslTransformations", fileName);
-      return XslTransformation.Create (fullPath);
-    }
-
-    private XDocument GetSourceXml (MethodBase methodBase)
-    {
-      return XDocument.Load (GetFileName (methodBase, "Source", "xml"), LoadOptions.SetBaseUri | LoadOptions.SetLineInfo);
-    }
-
-    private XDocument GetResultXml (MethodBase methodBase)
-    {
-      return XDocument.Load (GetFileName (methodBase, "Result", "xml"), LoadOptions.SetBaseUri | LoadOptions.SetLineInfo);
-    }
-
-    private string GetFileName (MethodBase methodBase, string suffix, string extension)
-    {
-      var assemblyName = methodBase.DeclaringType.Assembly.GetName().Name;
-      var namespaceName = methodBase.DeclaringType.Namespace;
-      Debug.Assert (namespaceName != null);
-
-      var folderName = namespaceName.Remove (0, assemblyName.Length).Split (new[] { '.' }, StringSplitOptions.RemoveEmptyEntries);
-      var fileName = methodBase.Name + suffix + "." + extension;
-
-      var fullPath = Path.Combine (folderName.Concat (new[] { fileName }).ToArray());
-      Assert.That (File.Exists (fullPath), "File not found: {0}", fullPath);
-
-      return fullPath;
     }
   }
 }
---
+++ Tests/XslTransformations/Docx2PlainXml/TestBase.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Xml.Linq;
+using DocxConverter.XslTransformations;
+using Jolt.Testing.Assertions.NUnit.SyntaxHelpers;
+using NUnit.Framework;
+
+namespace Tests.XslTransformations.Docx2PlainXml
+{
+  public abstract class TestBase
+  {
+    private XslTransformation _transformation;
+
+    protected abstract XslTransformation CreateXslTransformation ();
+
+    [SetUp]
+    public void SetUp ()
+    {
+      _transformation = CreateXslTransformation();
+    }
+
+    protected void RunAssertion (MethodBase currentMethod)
+    {
+      var source = GetSourceXml (currentMethod);
+      var expected = GetResultXml (currentMethod);
+      var actual = _transformation.Transform (source);
+      using (var actualReader = actual.CreateReader())
+      {
+        using (var expectedReader = expected.CreateReader())
+        {
+          Assert.That (actualReader, IsXml.EquivalentTo (expectedReader));
+        }
+      }
+    }
+
+    protected XslTransformation CreateXslTransformation (string fileName)
+    {
+      var fullPath = Path.Combine ("XslTransformations", fileName);
+      return XslTransformation.Create (fullPath);
+    }
+
+    private XDocument GetSourceXml (MethodBase methodBase)
+    {
+      return XDocument.Load ((string) GetFileName (methodBase, "Source", "xml"), LoadOptions.SetBaseUri | LoadOptions.SetLineInfo);
+    }
+
+    private XDocument GetResultXml (MethodBase methodBase)
+    {
+      return XDocument.Load ((string) GetFileName (methodBase, "Result", "xml"), LoadOptions.SetBaseUri | LoadOptions.SetLineInfo);
+    }
+
+    private string GetFileName (MethodBase methodBase, string suffix, string extension)
+    {
+      var assemblyName = methodBase.DeclaringType.Assembly.GetName().Name;
+      var namespaceName = methodBase.DeclaringType.Namespace;
+      Debug.Assert (namespaceName != null);
+
+      var folderName = namespaceName.Remove (0, assemblyName.Length).Split (new[] { '.' }, StringSplitOptions.RemoveEmptyEntries);
+      var fileName = methodBase.Name + suffix + "." + extension;
+
+      var fullPath = Path.Combine (folderName.Concat (new[] { fileName }).ToArray());
+      Assert.That (File.Exists (fullPath), "File not found: {0}", fullPath);
+
+      return fullPath;
+    }
+  }
+}
